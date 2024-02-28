ae27ffc77ccd18e91e3034234151956ceac3c172
Ben DiFrancesco <ben@scopelift.co>
2024-02-28 08:15:03 -0500

    Add access control and initialize roles

--- src/ZkTokenV1.sol
+++ src/ZkTokenV1.sol
@@ -1,12 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {ERC20VotesUpgradeable} from
   "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract ZkTokenV1 is Initializable, ERC20VotesUpgradeable {
-  function initialize() public initializer {
+contract ZkTokenV1 is Initializable, ERC20VotesUpgradeable, AccessControlUpgradeable {
+
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+  bytes32 public constant MINTER_ADMIN_ROLE = keccak256("MINTER_ADMIN_ROLE");
+  bytes32 public constant BURNER_ADMIN_ROLE = keccak256("BURNER_ADMIN_ROLE");
+
+  function initialize(address _admin) public initializer {
     __ERC20_init("zkSync", "ZK");
+    _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    _grantRole(MINTER_ADMIN_ROLE, _admin);
+    _grantRole(BURNER_ADMIN_ROLE, _admin);
+    _setRoleAdmin(MINTER_ROLE, MINTER_ADMIN_ROLE);
+    _setRoleAdmin(BURNER_ROLE, BURNER_ADMIN_ROLE);
   }
 }
--- test/ZkTokenV1.t.sol
+++ test/ZkTokenV1.t.sol
@@ -8,26 +8,81 @@ import {ZkTokenV1} from "src/ZkTokenV1.sol";
 contract ZkTokenV1Test is Test {
   ZkTokenV1 token;
   address proxyOwner = makeAddr("Proxy Owner");
+  address admin = makeAddr("Admin");
+
+  // Placed here for convenience in tests. Must match the constants in the implementation.
+  bytes32 public DEFAULT_ADMIN_ROLE = 0x00;
+  bytes32 public MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public BURNER_ROLE = keccak256("BURNER_ROLE");
+  bytes32 public MINTER_ADMIN_ROLE = keccak256("MINTER_ADMIN_ROLE");
+  bytes32 public BURNER_ADMIN_ROLE = keccak256("BURNER_ADMIN_ROLE");
 
   function setUp() public {
     address _proxy =
-      Upgrades.deployTransparentProxy("ZkTokenV1.sol", proxyOwner, abi.encodeCall(ZkTokenV1.initialize, ()));
+      Upgrades.deployTransparentProxy("ZkTokenV1.sol", proxyOwner, abi.encodeCall(ZkTokenV1.initialize, (admin)));
 
     token = ZkTokenV1(_proxy);
   }
 }
 
 contract Initialize is ZkTokenV1Test {
-  function test_InitializesTheTokenWithTheCorrectNameAndSymbolWhenDeployedViaUpgrades() public {
+  function test_InitializesTheTokenWithTheCorrectConfigurationWhenDeployedViaUpgrades() public {
     assertEq(token.symbol(), "ZK");
     assertEq(token.name(), "zkSync");
+
+    // The admin has all three administrative roles after initialization
+    assertTrue(token.hasRole(DEFAULT_ADMIN_ROLE, admin));
+    assertTrue(token.hasRole(MINTER_ADMIN_ROLE, admin));
+    assertTrue(token.hasRole(BURNER_ADMIN_ROLE, admin));
+
+    // The administrative roles have the proper association with each role
+    assertEq(token.getRoleAdmin(MINTER_ROLE), MINTER_ADMIN_ROLE);
+    assertEq(token.getRoleAdmin(BURNER_ROLE), BURNER_ADMIN_ROLE);
+
+    // The administrative role of the administrative roles are the default admin
+    assertEq(token.getRoleAdmin(MINTER_ADMIN_ROLE), DEFAULT_ADMIN_ROLE);
+    assertEq(token.getRoleAdmin(BURNER_ADMIN_ROLE), DEFAULT_ADMIN_ROLE);
+
+    // The default administrative role self administers
+    assertEq(token.getRoleAdmin(DEFAULT_ADMIN_ROLE), DEFAULT_ADMIN_ROLE);
   }
 
-  function test_InitializesTheTokenWithTheCorrectNameAndSymbolWhenCalledDirectly() public {
+  function testFuzz_InitializesTheTokenWithTheCorrectConfigurationWhenCalledDirectly(address _admin) public {
     ZkTokenV1 _token = new ZkTokenV1();
-    _token.initialize();
+    _token.initialize(_admin);
 
+    // Same assertions as upgradeable deploy test
     assertEq(_token.symbol(), "ZK");
     assertEq(_token.name(), "zkSync");
+    assertTrue(_token.hasRole(DEFAULT_ADMIN_ROLE, _admin));
+    assertTrue(_token.hasRole(MINTER_ADMIN_ROLE, _admin));
+    assertTrue(_token.hasRole(BURNER_ADMIN_ROLE, _admin));
+    assertEq(_token.getRoleAdmin(MINTER_ROLE), MINTER_ADMIN_ROLE);
+    assertEq(_token.getRoleAdmin(BURNER_ROLE), BURNER_ADMIN_ROLE);
+    assertEq(_token.getRoleAdmin(MINTER_ADMIN_ROLE), DEFAULT_ADMIN_ROLE);
+    assertEq(_token.getRoleAdmin(BURNER_ADMIN_ROLE), DEFAULT_ADMIN_ROLE);
+    assertEq(_token.getRoleAdmin(DEFAULT_ADMIN_ROLE), DEFAULT_ADMIN_ROLE);
+  }
+
+  function testFuzz_InitializesTheTokenSuchThatTheAdminRolesCanGrantTheRoleToOthers(address _newAdmin) public {
+    vm.assume(_newAdmin != admin);
+
+    // The default admin can add another default admin
+    vm.prank(admin);
+    token.grantRole(DEFAULT_ADMIN_ROLE, _newAdmin);
+    assertTrue(token.hasRole(DEFAULT_ADMIN_ROLE, _newAdmin));
+
+    // The new default admin can add other minter/burner admins
+    vm.prank(_newAdmin);
+    token.grantRole(MINTER_ADMIN_ROLE, _newAdmin);
+    assertTrue(token.hasRole(MINTER_ADMIN_ROLE, _newAdmin));
+    vm.prank(_newAdmin);
+    token.grantRole(BURNER_ADMIN_ROLE, _newAdmin);
+    assertTrue(token.hasRole(BURNER_ADMIN_ROLE, _newAdmin));
+
+    // The new default admin can revoke the default role from the original admin
+    vm.prank(_newAdmin);
+    token.revokeRole(DEFAULT_ADMIN_ROLE, admin);
+    assertFalse(token.hasRole(DEFAULT_ADMIN_ROLE, admin));
   }
 }
