947cd68215cccaab2d17d50426a78fea83fbcf81
John Feras <jferas@ferasinfotech.com>
2024-03-14 12:28:10 -0400

    Added permit functionality (#11)

--- test/ZkTokenV1.t.sol
+++ test/ZkTokenV1.t.sol
@@ -7,6 +7,8 @@ import {ZkTokenV1, Initializable} from "src/ZkTokenV1.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 import {ERC1967Utils} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
 import {ZkTokenFakeV2} from "test/harnesses/ZkTokenFakeV2.sol";
+import {ERC20PermitUpgradeable} from
+  "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
 contract ZkTokenV1Test is Test {
   ZkTokenV1 token;
@@ -494,5 +496,178 @@ contract Upgrade is ZkTokenV1Test {
     _tokenV2.initialize(_minter, initMintReceiver, INITIAL_MINT_AMOUNT);
     vm.expectRevert(Initializable.InvalidInitialization.selector);
     _tokenV2.initializeFakeV2(_nextValue);
+  }
+}
+
+contract Permit is ZkTokenV1Test {
+  bytes32 constant PERMIT_TYPEHASH =
+    keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+  function testFuzz_PerformsTheApprovalByCallingPermitThenPerformsTransfer(
+    uint256 _ownerPrivateKey,
+    uint256 _amount,
+    address _spender,
+    address _receiver,
+    uint256 _deadline
+  ) public {
+    vm.assume(_spender != address(0) && _receiver != address(0) && _receiver != initMintReceiver);
+    _deadline = bound(_deadline, block.timestamp + 1, type(uint256).max);
+    _ownerPrivateKey = bound(_ownerPrivateKey, 1, 100e18);
+    address _owner = vm.addr(_ownerPrivateKey);
+    _amount = bound(_amount, 0, MAX_MINT_SUPPLY);
+
+    vm.prank(admin);
+    token.grantRole(MINTER_ROLE, _owner);
+    vm.prank(_owner);
+    token.mint(_owner, _amount);
+
+    // verify the owner has the expected balance
+    assertEq(token.balanceOf(_owner), _amount);
+
+    bytes32 _message =
+      keccak256(abi.encode(PERMIT_TYPEHASH, _owner, _spender, _amount, token.nonces(_owner), _deadline));
+
+    bytes32 _messageHash = keccak256(abi.encodePacked("\x19\x01", token.DOMAIN_SEPARATOR(), _message));
+    (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(_ownerPrivateKey, _messageHash);
+
+    vm.prank(_spender);
+    token.permit(_owner, _spender, _amount, _deadline, _v, _r, _s);
+
+    vm.prank(_spender);
+    token.transferFrom(_owner, _receiver, _amount);
+
+    // verify the receiver has the expected balance
+    assertEq(token.balanceOf(_receiver), _amount);
+
+    // verify the owner has the zero balance
+    assertEq(token.balanceOf(_owner), 0);
+  }
+
+  function testFuzz_RevertIf_ThePermitSignatureIsInvalid(
+    address _notOwner,
+    uint256 _ownerPrivateKey,
+    uint256 _amount,
+    address _spender,
+    address _receiver,
+    uint256 _deadline
+  ) public {
+    vm.assume(_spender != address(0) && _receiver != address(0) && _notOwner != address(0));
+    _deadline = bound(_deadline, block.timestamp + 1, type(uint256).max);
+    _ownerPrivateKey = bound(_ownerPrivateKey, 1, 100e18);
+    address _owner = vm.addr(_ownerPrivateKey);
+    vm.assume(_notOwner != _owner);
+    _amount = bound(_amount, 0, MAX_MINT_SUPPLY);
+
+    vm.prank(admin);
+    token.grantRole(MINTER_ROLE, _owner);
+    vm.prank(_owner);
+    token.mint(_owner, _amount);
+
+    // verify the owner has the expected balance
+    assertEq(token.balanceOf(_owner), _amount);
+
+    bytes32 _message =
+      keccak256(abi.encode(PERMIT_TYPEHASH, _notOwner, _spender, _amount, token.nonces(_notOwner), _deadline));
+
+    bytes32 _messageHash = keccak256(abi.encodePacked("\x19\x01", token.DOMAIN_SEPARATOR(), _message));
+    (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(_ownerPrivateKey, _messageHash);
+
+    // verify the permit signature is invalid
+    vm.prank(_spender);
+    vm.expectRevert(abi.encodeWithSelector(ERC20PermitUpgradeable.ERC2612InvalidSigner.selector, _owner, _notOwner));
+    token.permit(_notOwner, _spender, _amount, _deadline, _v, _r, _s);
+  }
+
+  function testFuzz_PerformsTheApprovalByCallingPermitThenPerformsTransferWithoutProxy(
+    uint256 _ownerPrivateKey,
+    uint256 _amount,
+    address _spender,
+    address _receiver,
+    uint256 _deadline
+  ) public {
+    vm.assume(_spender != address(0) && _receiver != address(0) && _receiver != initMintReceiver);
+    _deadline = bound(_deadline, block.timestamp + 1, type(uint256).max);
+    _ownerPrivateKey = bound(_ownerPrivateKey, 1, 100e18);
+    address _owner = vm.addr(_ownerPrivateKey);
+    _amount = bound(_amount, 0, MAX_MINT_SUPPLY);
+
+    token = new ZkTokenV1();
+    token.initialize(admin, initMintReceiver, INITIAL_MINT_AMOUNT);
+
+    vm.prank(admin);
+    token.grantRole(MINTER_ROLE, _owner);
+    vm.prank(_owner);
+    token.mint(_owner, _amount);
+
+    // verify the owner has the expected balance
+    assertEq(token.balanceOf(_owner), _amount);
+
+    bytes32 _message =
+      keccak256(abi.encode(PERMIT_TYPEHASH, _owner, _spender, _amount, token.nonces(_owner), _deadline));
+
+    bytes32 _messageHash = keccak256(abi.encodePacked("\x19\x01", token.DOMAIN_SEPARATOR(), _message));
+    (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(_ownerPrivateKey, _messageHash);
+
+    vm.prank(_spender);
+    token.permit(_owner, _spender, _amount, _deadline, _v, _r, _s);
+
+    vm.prank(_spender);
+    token.transferFrom(_owner, _receiver, _amount);
+
+    // verify the receiver has the expected balance
+    assertEq(token.balanceOf(_receiver), _amount);
+
+    // verify the owner has the zero balance
+    assertEq(token.balanceOf(_owner), 0);
+  }
+
+  function testFuzz_RevertIf_ThePermitSignatureIsInvalidWithouProxy(
+    address _notOwner,
+    uint256 _ownerPrivateKey,
+    uint256 _amount,
+    address _spender,
+    address _receiver,
+    uint256 _deadline
+  ) public {
+    vm.assume(_spender != address(0) && _receiver != address(0) && _notOwner != address(0));
+    _deadline = bound(_deadline, block.timestamp + 1, type(uint256).max);
+    _ownerPrivateKey = bound(_ownerPrivateKey, 1, 100e18);
+    address _owner = vm.addr(_ownerPrivateKey);
+    vm.assume(_notOwner != _owner);
+    _amount = bound(_amount, 0, MAX_MINT_SUPPLY);
+
+    token = new ZkTokenV1();
+    token.initialize(admin, initMintReceiver, INITIAL_MINT_AMOUNT);
+
+    vm.prank(admin);
+    token.grantRole(MINTER_ROLE, _owner);
+    vm.prank(_owner);
+    token.mint(_owner, _amount);
+
+    // verify the owner has the expected balance
+    assertEq(token.balanceOf(_owner), _amount);
+
+    bytes32 _message =
+      keccak256(abi.encode(PERMIT_TYPEHASH, _notOwner, _spender, _amount, token.nonces(_notOwner), _deadline));
+
+    bytes32 _messageHash = keccak256(abi.encodePacked("\x19\x01", token.DOMAIN_SEPARATOR(), _message));
+    (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(_ownerPrivateKey, _messageHash);
+
+    // verify the permit signature is invalid
+    vm.prank(_spender);
+    vm.expectRevert(abi.encodeWithSelector(ERC20PermitUpgradeable.ERC2612InvalidSigner.selector, _owner, _notOwner));
+    token.permit(_notOwner, _spender, _amount, _deadline, _v, _r, _s);
+  }
+}
+
+/// @dev Used to test the ERC20VotesUpgradeable nonces method for coverage purposes as it is inherited from
+/// ERC20VotesUpgradeable
+contract Nonces is ZkTokenV1Test {
+  function testFuzz_NoncesWithoutProxy(address _owner) public {
+    vm.assume(_owner != address(0));
+    token = new ZkTokenV1();
+    token.initialize(admin, initMintReceiver, INITIAL_MINT_AMOUNT);
+    uint256 _nonce = token.nonces(_owner);
+    assertEq(token.nonces(_owner), _nonce);
   }
 }
