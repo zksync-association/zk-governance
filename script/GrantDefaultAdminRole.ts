1ec46fe2d78011eb055a7bcfd6642a28bc9fb1ac
Ben DiFrancesco <ben@scopelift.co>
2024-05-21 15:11:56 -0400

    Grant default admin role to interim admin

--- script/GrantDefaultAdminRole.ts
+++ script/GrantDefaultAdminRole.ts
@@ -7,7 +7,7 @@ import { getTokenContract } from './utils'
 // The TOKEN_ADDRESS below is derived from the output of the DeployZkTokenV1.ts script, using hardhat account 0 as the deployer.
 // For local testing purposes, the NEW_DEFAULT_ADMIN is set to local hardhat account 3.
 const TOKEN_ADDRESS = "0x5A7d6b2F92C77FAD6CCaBd7EE0624E64907Eaf3E";
-const NEW_DEFAULT_ADMIN = "0xd986b0cB0D1Ad4CCCF0C4947554003fC0Be548E9";
+const NEW_DEFAULT_ADMIN = "0x3cFc0e11D88B38A7577DAB36f3a8E5e8538a8C22";
 
 async function main() {
   dotEnvConfig();
