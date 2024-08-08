870153cf1c780ef36127691adcf5f92d85467bc5
Ben DiFrancesco <ben@scopelift.co>
2024-08-08 15:33:34 -0400

    Perform era mainnet deployment of L2 governance contracts

--- l2-contracts/script/DeployZkProtocolGovernor.ts
+++ l2-contracts/script/DeployZkProtocolGovernor.ts
@@ -7,12 +7,13 @@ import * as hre from "hardhat";
 // to. The values used in the script at the time of deployment can be checked in along with the deployment artifacts
 // produced by running the scripts.
 const contractName = "ZkProtocolGovernor";
-const tokenAddress = "0x69e5DC39E2bCb1C17053d2A4ee7CAEAAc5D36f96";  // TODO: We'll need to deploy this contract first to get the actual address
-const votingDelay = 60 * 15; // For test purposes, 15 minutes
-const votingPeriod = 60 * 15; // For test purposes, 15 minutes
-const proposalThreshold = 10; // For testing purposes, actual deployment will need real values
-const initialQuorum = 100; // For testing purposes, actual deployment will need real values
-const initialLateQuorum = 60 * 15; // For test purposes, 15 minutes
+const tokenAddress = "0x5A7d6b2F92C77FAD6CCaBd7EE0624E64907Eaf3E";
+const votingDelay = 7 * 24 * 60 * 60; // 7 days
+const votingPeriod = 7 * 24 * 60 * 60; // 7 days
+const proposalThreshold = "21000000000000000000000000"; // Raw decimals for 21 Million (0.1% of supply)
+const initialQuorum = "630000000000000000000000000"; // Raw decimals for 630 Million (3% of supply)
+const initialLateQuorum = 7 * 24 * 60 * 60; // 7 days
+const timelockDelay = 0; // No delay enforced from timelock
 
 async function main() {
   dotEnvConfig();
@@ -29,7 +30,7 @@ async function main() {
   console.log(`Deploying ${contractName} TimelockController contract...`);
   const timelockContract = await deployer.loadArtifact("TimelockController");
   const adminAddress = await zkWallet.getAddress();
-  const timelockConstructorArgs = [0, [], [], adminAddress];
+  const timelockConstructorArgs = [timelockDelay, [], [], adminAddress];
   const timelock = await deployer.deploy(timelockContract, timelockConstructorArgs);
   const timeLockAddress = await timelock.getAddress();
   console.log(`${contractName} TimelockController contract was deployed to ${timeLockAddress}`);
@@ -46,11 +47,11 @@ async function main() {
   const theToken = await protocolGovernor.token();
   console.log(`The ${contractName} Token is set to: ${theToken}`);
 
-  (await timelock.grantRole(await timelock.PROPOSER_ROLE(), contractAddress)).wait();
-  (await timelock.grantRole(await timelock.CANCELLER_ROLE(), contractAddress)).wait();
-  (await timelock.grantRole(await timelock.EXECUTOR_ROLE(), contractAddress)).wait();
+  await (await timelock.grantRole(await timelock.PROPOSER_ROLE(), contractAddress)).wait();
+  await (await timelock.grantRole(await timelock.CANCELLER_ROLE(), contractAddress)).wait();
+  await (await timelock.grantRole(await timelock.EXECUTOR_ROLE(), contractAddress)).wait();
   console.log(`Timelock PROPOSER, CANCELLER, and EXECUTOR roles granted to ${contractName} contract`);
-  (await timelock.renounceRole(await timelock.TIMELOCK_ADMIN_ROLE(), adminAddress)).wait();
+  await (await timelock.renounceRole(await timelock.TIMELOCK_ADMIN_ROLE(), adminAddress)).wait();
   console.log(`ADMIN Role renounced for ${contractName} TimelockController contract (now self-administered)`);
 }
 
