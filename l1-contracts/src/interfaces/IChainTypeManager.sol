a9512b01ef3780d8ed28eea1b867403f0d5ccdb9
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-08-13 10:52:36 +0200

    Keep PUH + TestnetPUH changes and required interfaces; everything else from master

---
+++ l1-contracts/src/interfaces/IChainTypeManager.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+/// @author Matter Labs
+/// @custom:security-contact security@matterlabs.dev
+interface IChainTypeManager {
+    function freezeChain(uint256 _chainId) external;
+
+    function unfreezeChain(uint256 _chainId) external;
+
+    function validatorTimelock() external view returns (address);
+}
